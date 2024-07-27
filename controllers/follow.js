a856584e33893d9af14a67e9c985743dc3019a78
Andrés Darín <andres.darin05@gmail.com>
2024-07-27 17:07:55 -0300

    Follow | Unfollow

--- controllers/follow.js
+++ controllers/follow.js
@@ -9,8 +9,78 @@ const pruebaFollow = (req, res) => {
 }
 
 //Accion de guardar un follow (accion de Seguir)
+const save = async (req, res) => {
+    try {
+        //conseguir datos por body
+        const params = req.body;
 
-//Accion de borrar un follow (dejar de seguir)
+        //sacar id del usuario identificado
+        const identity = req.user;
+
+        //crear objeto con modelo follow
+        let userToFollow = new Follow({
+            user: identity.id,
+            followed: params.followed
+        });
+
+        //guardar objeto en base de datos
+        const followStored = await userToFollow.save();
+
+        if (!followStored) {
+            return res.status(500).json({
+                status: "error",
+                message: 'no se ha podido seguir al usuario'
+            });
+        }
+
+        return res.status(200).send({
+            status: 'success',
+            identity: req.user,
+            userToFollow
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+}
+
+const unfollow = async (req, res) => {
+    try {
+        // Recoger el id del usuario identificado
+        const userId = req.user.id;
+
+        // Recoger el id del usuario que sigo y quiero dejar de seguir
+        const followedId = req.params.id;
+
+        // Buscar la coincidencia y eliminar
+        const followRemoved = await Follow.findOneAndDelete({
+            user: userId,
+            followed: followedId
+        });
+
+        if (!followRemoved) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'No se ha encontrado el follow para eliminar'
+            });
+        }
+
+        return res.status(200).send({
+            status: 'success',
+            message: 'Follow eliminado correctamente',
+            followRemoved
+        });
+    } catch (error) {
+        return res.status(500).send({
+            status: 'error',
+            message: error.message
+        });
+    }
+}
 
 //Lista de usuarios a los que estoy siguiendo
 
@@ -18,5 +88,7 @@ const pruebaFollow = (req, res) => {
 
 //exportar acciones
 module.exports = {
-    pruebaFollow
+    pruebaFollow,
+    save,
+    unfollow
 }
