394f311865ff583d711ce350a913f6348e15215b
Andrés Darín <andres.darin05@gmail.com>
2025-08-24 17:30:52 -0300

    Update batch file to include additional project startup commands

--- arrancar-proyecto.bat
+++ arrancar-proyecto.bat
@@ -1,2 +1,5 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:/data/db"
 start cmd.exe /k "cd C:\Users\Usuario\Desktop\React Desarrollo\api-rest-red-social && npm start"
+start cmd.exe /k "cd C:\Users\Usuario\Documents\Dev\React Desarrollo\The True React\red-social-front-end &&  npm run dev" 
+start "" /b code "C:\Users\Usuario\Desktop\React Desarrollo\api-rest-red-social" 
+start "" /b code "C:\Users\Usuario\Documents\Dev\React Desarrollo\The True React\red-social-front-end" 
